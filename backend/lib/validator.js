fd6878afd165496b700ef71571f836795a63ea70
Mark Havryliv <mark@scoreaddiction.com>
2021-09-17 14:06:49 +1000

    validator and basic tests working

--- backend/lib/validator.js
+++ backend/lib/validator.js
@@ -2,21 +2,50 @@
   Validates input before sending to Scalapay endpoint
 */
 
+// Maximum string length for any field, as a default safety check
+const MaxStrLen = 50;
+
+const ErrMsgMissingField = "Missing data";
+const ErrMsgFieldInvalid = "Invalid data";
+
 // An array of required objects and their fields, related to personal info
 const requiredPersonalObjects = [
-  {name: 'totalAmount', fields: ['amount', 'currency']},
-  {name: 'consumer', fields: ['givenNames', 'surname']},
-  {name: 'shipping', fields: ['countryCode', 'name', 'postcode', 'line1']}
+  {name: 'totalAmount', fields: 
+    [{name: 'amount', isValid: (val) => {return val >= 0}}, 
+     {name: 'currency', isValid: (code) => {return code.length === 3}}]
+  },
+  {name: 'consumer', fields: [{name: 'givenNames'}, {name: 'surname'}]},
+  {name: 'shipping', fields: 
+    [{name: 'countryCode', isValid: (code) => {return code.length === 2}},
+    {name: 'name'}, {name: 'postcode'}, {name: 'line1'}]
+  }
 ]
 
 // An array of required info for an item
 const requiredItemPurchaseInfo = [
-  {name: 'quantity', isValid: (val) => {return Number.isInteger(val)}},
-  {name: 'price', fields: ['amount', 'currency']},
+  {name: 'quantity', isValid: (val) => {return Number.isInteger(val) && val >= 0}},
+  {name: 'price', fields:
+   [{name: 'amount', isValid: (val) => {return val >= 0}}, {name: 'currency'}]
+  },
   {name: 'name'},
   {name: 'category'},
   {name: 'sku'}
 ]
+
+/*
+  Checks the validity of a field by testing if it has a built-in isValid function
+  and if not, just applying a standard string length test
+*/
+const isValid = (field, val) => {
+  // If the field has a built-in validator function, use that
+  if(field.isValid) {
+    const res = field.isValid(val);
+    return res;
+  }
+  else {
+    return val.length <= MaxStrLen;
+  }
+}
 
 /*
   Asseses whether @order contains all required fields, and checks where a field is required
@@ -25,9 +54,44 @@ const requiredItemPurchaseInfo = [
   and problems.
 */
 const validate = (order) => {
-  if(order === {}) {
+  // Keep record of errors
+  let errors = [];
+  // Check for all required field objects
+  requiredPersonalObjects.forEach(fieldObj => {
+    // Check the order contains an object for this field object
+    if(!order[fieldObj.name]) {
+      errors.push({field: fieldObj.name, msg: ErrMsgMissingField})
+      return;
+    }
+    const orderObj = order[fieldObj.name];
+    // If this field object has no child fields, simply check it's validity and 
+    // continue the loop
+    if(!fieldObj.fields) {
+      if(!isValid(fieldObj, orderObj)) {
+        errors.push({field: fieldObj.name, msg: ErrMsgFieldInvalid})
+        return;
+      }
+    }
+    // Else, loop through the field's child fields and check their existence and validity
+    fieldObj.fields.forEach(childField => {
+      const orderChildVal = orderObj[childField.name];
+      // If the order doesn't contain the required child field, add and error and continue
+      if(!orderChildVal) {
+        errors.push({field: fieldObj.name + "." + childField.name, msg: ErrMsgMissingField})
+        return;
+      }
+      // Check the chield field's validity
+      if(!isValid(childField, orderChildVal)) {
+        errors.push({field: fieldObj.name + "." + childField.name, msg: ErrMsgFieldInvalid})
+        return;
+      }
+    });
+  });
+
+  if(errors.length !== 0) {
     return {
-      valid: false
+      valid: false,
+      errors: errors
     }  
   }
   else {
@@ -40,3 +104,7 @@ const validate = (order) => {
 
 
 exports.validate = validate;
+
+// Export these to help build test cases
+exports.requiredPersonalObjects = requiredPersonalObjects;
+exports.requiredItemPurchaseInfo = requiredItemPurchaseInfo;
--- backend/tests/validator.test.js
+++ backend/tests/validator.test.js
@@ -1,6 +1,71 @@
 const validator = require('../lib/validator');
 
+// Sanity checks of the validator
 test('Empty data is invalid', () => {
   const res = validator.validate({});
-  expect(res.valid === false);
+  expect(res.valid).toBe(false);
+  console.log(res.errors);
 });
+
+// Test with a valid order
+test('Valid order', () => {
+  const order = orderBuilder();
+  const res = validator.validate(order);
+  expect(res.valid).toBe(true);
+});
+
+test('Invalid order: 1 top-level field missing', () => {
+  const order = orderBuilder();
+  delete order.totalAmount;
+  const res = validator.validate(order);
+  expect(res.valid).toBe(false);
+  expect(res.errors.length).toBe(1);
+  expect(res.errors[0].field).toBe('totalAmount');
+});
+
+test('Invalid order: 1 child field missing', () => {
+  const order = orderBuilder();
+  delete order.totalAmount.amount;
+  const res = validator.validate(order);
+  expect(res.valid).toBe(false);
+  expect(res.errors.length).toBe(1);
+  expect(res.errors[0].field).toBe('totalAmount.amount');
+});
+
+/*
+Utility function to build orders for testing the validator
+*/
+const orderBuilder = () => {
+  // Initialise the order - amount is updated once all items have been generated
+  let order = {
+    totalAmount: {amount: 0, currency: "EUR"},
+    consumer: {givenNames: "Mark Patrick", surname: "Havryliv"},
+    shipping: {countryCode: "AU", name: "Mark Havryliv", postcode: "2539", line1: "95 Garside Road"}
+  };
+  // Initialise the items
+  order.items = [];
+  // Generate a random number of orders between 1-10
+  const numItems = Math.floor(Math.random() * 10) + 1;
+  // Keep a running sum of the total price
+  let totalAmount = 0;
+  for(let i = 0; i < numItems; ++i) {
+    let item = {};
+    item.quantity = Math.floor(Math.random() * 20) + 1;
+    item.price = {
+      amount: parseFloat((Math.random() * 200).toFixed(2)),
+      currency: "EUR"
+    }
+    totalAmount += item.price.amount;
+    item.name = "Converse Chuck Taylors";
+    item.category = "Street shoes";
+    item.sku = "12341234"
+
+    // Add this item to the order
+    order.items.push(item);
+  }
+
+  // Update the total amount
+  order.totalAmount.amount = totalAmount;
+
+  return order;
+}
